9895498231e6b1cdf4b14a51278ec04b3114185d
radness <yhs5128@gmail.com>
2020-02-02 16:00:02 +0900

    setting react-router

---
+++ src/shared/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                App
+            </div>
+        );
+    }
+}
+
+App.propTypes = propTypes;
+App.defaultProps = defaultProps;
+
+export default App;
